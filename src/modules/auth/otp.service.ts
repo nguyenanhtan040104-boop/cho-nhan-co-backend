19652c626deb23611d544601d3b7018a2094fdce
Nguyen Anh Tan <nguyenanhtan@gmail.com>
2026-05-06 22:30:44 +0700

    fix: fallback to SMTP port 587

--- src/modules/auth/otp.service.ts
+++ src/modules/auth/otp.service.ts
@@ -8,7 +8,9 @@ export class OtpService {
 
   constructor(private prisma: PrismaService) {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
